a9ef3c791f0d7c32b3b6f1bde6628e1f4de6e264
AndroidKimyona <bark73966@gmail.com>
2026-05-18 16:05:00 -0300

    socorro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-<<<<<<< HEAD
 
 android {
     namespace = "android.kimyona.jammer"
@@ -46,37 +45,4 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-=======
-android {
-    namespace = "android.kimyona.jammer"
-    compileSdk = 34
-    defaultConfig {
-        applicationId = "android.kimyona.jammer"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.arthenica:ffmpeg-kit-full:6.0-2")
-    implementation("dev.rikka.shizuku:api:13.1.5")
-    implementation("dev.rikka.shizuku:provider:13.1.5")
->>>>>>> branch 'main' of https://github.com/AndroidKimyona/Music-player.git
 }
